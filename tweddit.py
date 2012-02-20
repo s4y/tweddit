53de74e4fa98fb8d83a6b8d23d812ea57ad294be
Sidney San Martín <s@sidneysm.com>
2012-02-20 18:44:27 -0500

    Refactor to use classes better, run the HTTP stream in another thread

--- tweddit.py
+++ tweddit.py
@@ -1,43 +1,63 @@
-import pycurl, json, secrets
+import secrets, threading
+from operator import itemgetter
 
 STREAM_URL = "https://stream.twitter.com/1/statuses/sample.json"
 
-urls = {}
 
-class Client:
-	def __init__(self):
+
+class TwitterClient(threading.Thread):
+	def __init__(self, handler):
+		import pycurl
 		self.buffer = ""
+		self.handler = handler
 		self.conn = pycurl.Curl()
 		self.conn.setopt(pycurl.USERPWD, "%s:%s" % (secrets.username, secrets.password))
+		self.conn.setopt(pycurl.HEADER, 0)
+		self.conn.setopt(pycurl.NOSIGNAL, 1)
 		self.conn.setopt(pycurl.URL, STREAM_URL)
 		self.conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
+		threading.Thread.__init__(self)
+		self.daemon = True
+	def run(self):
 		self.conn.perform()
-
 	def on_receive(self, data):
-		import operator
-		import datetime
+		from json import loads
 		self.buffer += data
 		if data.endswith("\r\n") and self.buffer.strip():
-			content = json.loads(self.buffer)
+			self.handler(loads(self.buffer))
 			self.buffer = ""
 
-			if 'entities' in content:
-				tweet_urls = [url['expanded_url'] or url['url'] for url in content['entities']['urls']]
-				for k, url in enumerate(tweet_urls):
-					urls[url] = ((urls[url][0] + 1 if (url in urls) else 1), datetime.datetime.now())
-					print '%d: %s (total: %d)' % (urls[url][0], url, len(urls))
-				if len(urls) > 15000:
-					urls_sorted = urls.items()
-					urls_sorted.sort(key=operator.itemgetter(1), reverse=True)
-					urls_sorted.sort(reverse=True)
-					for url_to_delete in urls_sorted[10000:]:
-						del urls[str(url_to_delete[0])]
-		if len(urls) % 50 == 0:
-				urls_sorted = urls.items()
-				urls_sorted.sort(key=operator.itemgetter(1), reverse=True)
-				print '\n-------------\n\n\n'
-				for url in urls_sorted[:10]:
-					print '%d: %s' % (url[1][0], url[0])
-				print '\n\n'
+class Tweddit():
+	def __init__(self):
+		self.urls = {}
+		self.client = TwitterClient(self.handle_tweet)
+	def start(self):
+		self.client.start();
+	def handle_tweet(self, tweet):
+		from datetime import datetime
+		if 'entities' in tweet:
+			tweet_urls = [url['expanded_url'] or url['url'] for url in tweet['entities']['urls']]
+			for url in tweet_urls:
+				self.urls[url] = ((self.urls[url][0] + 1 if (url in self.urls) else 1), datetime.now())
+				print '%d: %s (total: %d)' % (self.urls[url][0], url, len(self.urls))
+			if len(self.urls) > 15000:
+				self.prune()
+	def prune(self):
+		urls_sorted = self.urls.items()
+		urls_sorted.sort(key=itemgetter(1), reverse=True)
+		urls_sorted.sort(reverse=True)
+		for url_to_delete in urls_sorted[10000:]:
+			del self.urls[str(url_to_delete[0])]
 
-client = Client()
+if __name__ == '__main__':
+	tweddit = Tweddit()
+	tweddit.start()
+	e = threading.Event()
+	while True:
+		e.wait(10)
+		urls_sorted = tweddit.urls.items()
+		urls_sorted.sort(key=itemgetter(1), reverse=True)
+		print '\n-------------\n\n\n'
+		for url in urls_sorted[:10]:
+			print '%d: %s' % (url[1][0], url[0])
+		print '\n\n'
